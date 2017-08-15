63f2d7f1f079d4f5ac4ac4bea667be661d568a88
Darren Cotterill <darren.cotterill@gmail.com>
2017-08-15 19:45:33 +1000

    Add PiVideoStream class

--- camera/VideoStream.py
+++ camera/VideoStream.py
@@ -3,7 +3,7 @@ from imutils.video import WebcamVideoStream
 class VideoStream:
     def __init__(self, src=0, usePiCamera=False, resolution=(320, 240), framerate=32):
         if usePiCamera:
-            from pivideostream import PiVideoStream
+            from imutils.video.pivideostream import PiVideoStream
 
             self.stream = PiVideoStream(resolution=resolution, framerate=framerate)
         else:
